16f71277f9157607f65fb27d8df33ae17421c6b8
lucyrush <lucy@codeinstitute.net>
2020-10-30 16:28:45 +0000

    Add unstyled footer

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
